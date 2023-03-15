e570ec1ff1fd6e9d38595a919c23b3bbf9b32806
Joanna <astronautnemo@gmail.com>
2023-03-15 12:12:05 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Index route - GET /plants
+  # Show route - GET /plants/:id
+  # Create route - POST /plants
+  resources :plants, only: [:index, :show, :create]
+
 end
